655ef8972c0c521f950875460a2161d5a634a1dc
kbsmith01 <kbsmith01@TS15849.ysu.local>
2018-02-02 12:13:46 -0500

    Method structures in place, main application runs without errors.

---
+++ src/java_class_prg_01_hw_01_pod_racing_app/Pod.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_class_prg_01_hw_01_pod_racing_app;
+
+/**
+ *
+ * @author kbsmith01
+ */
+public class Pod {
+    private int posX;
+    private int posY;
+    
+    public Pod(int x, int y, String direction, int width, int height) {
+        
+    }
+    
+    public void playerAt(int x, int y) {
+        
+    }
+    
+    public void move() {
+        
+    }
+    
+    public void randomChange() {
+        
+    }
+    
+    public boolean isCaught() {
+        return true;
+    }
+    
+    public int getX() {
+        return posX;
+    }
+    
+    public int getY() {
+        return posY;
+    }
+}
